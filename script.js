0e28d60fbbcd8476574a3ba519cabaef5f4189a0
Matheus Alonso <matheus.siqueiraalonso@gmail.com>
2026-04-22 21:33:13 -0300

    refactor: reorganiza estrutura de diretórios

--- script.js
+++ script.js
@@ -1,134 +1,195 @@
 let personagem = document.getElementById('personagem')
-let cenario = document.getElementById('cenario')
-
-
+let personagemImg = document.getElementById('personagemImg')
+let container = document.getElementById('container')
 let rodando = true
-
-document.addEventListener('keydown', function(event){
-    if(event.key === ' '){
-
-
-        personagem.classList.add('personagem-pular');
-
-        // remove depois que a animação termina
-        setTimeout(() => {
-            personagem.classList.remove('personagem-pular');
-
-        }, 600);
-    }
-    if (event.key === "ArrowDown") {
-        personagem.classList.add('personagem-abaixar')
-    }
-});
-
-document.addEventListener("keyup", function(event) {
-    personagem.classList.remove('personagem-abaixar');
-});
-
-
-
 let pontos = 0
 let dificuldade = 1
-let span = 1.5
-let classeObstaculo = 'obstaculo-mover1'
-let tipoObstaculo
+let spanPontuação = document.getElementById('pontuacao')
+let spanNivel = document.getElementById('nivel')
+let camada0 = document.getElementById('camada0')
+let podeColidir = true
+let podeMover = true
 
-function gerar() {
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowUp' && podeMover){
+        if(personagem.classList.contains('personagemSubir')){
+
+        }else{
+            personagem.classList.add('personagemSubir')
+            personagem.classList.remove('personagemDescer')
+            personagemImg.src = 'img/personagemSubindo.png'
+            setTimeout(() => {
+                personagemImg.src = "img/personagem.png";
+            }, 200);
+            podeColidir = false
+            setTimeout(() => {
+                podeColidir = true;
+            }, 70);
+        }
+
+    }
+    if(event.key === 'p'){
+       
+    }
+    if(event.key === 'ArrowDown' && podeMover){
+        if(personagem.classList.contains('personagemDescer')){
+
+        }else{
+            personagem.classList.add('personagemDescer')
+            personagem.classList.remove('personagemSubir')
+            personagemImg.src = 'img/personagemDescendo.png'
+            setTimeout(() => {
+                personagemImg.src = "img/personagem.png";
+            }, 200);
+            podeColidir = false
+            setTimeout(() => {
+                podeColidir = true;
+            }, 70);
+
+        }
+    }
+});
+
+
+
+function play(){
+    if(pontos == 20) dificuldade = 2
+    if(pontos == 40) dificuldade = 3
+    if(pontos == 60) dificuldade = 4
+    if(pontos == 80) dificuldade = 5
+    if(pontos == 100) dificuldade = 6
+    if(pontos == 150) dificuldade = 7
+
+
+    pontos++
+
+    document.getElementById('pontuacao').textContent = pontos
+
+    spanNivel.textContent = dificuldade
+    spanPontuação.textContent = pontos
+    let novo = document.createElement('div');
+    let novoImg = document.createElement('img')
+    novoImg.src = 'img/nave3.png'
+    novoImg.classList.add('obstaculoImg')
+    novo.appendChild(novoImg)
+    localObstaculo = gerarLocalObstaculo()
+
+
+    switch(dificuldade){
+        case 1:
+            frequenciaObstaculo = gerarFrequenciaObstaculo(2)            
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){                    
+                    novo.classList.add('obstaculo', 'obstaculoNivel1' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel1' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 1 * 1000);
+        break;
+        case 2:
+            camada0.style.backgroundColor = 'rgba(0, 0, 255, 0.106)'
+            frequenciaObstaculo = gerarFrequenciaObstaculo(2)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel2' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel2' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 1 * 1000);
+        break;
+        case 3:
+            camada0.style.backgroundColor = 'rgba(0, 13, 255, 0.232)'
+            frequenciaObstaculo = gerarFrequenciaObstaculo(2)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel3' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel3' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 1 * 1000);
+        break;
+        case 4:
+            camada0.style.backgroundColor = 'rgba(0, 0, 0, 0.357)'            
+            frequenciaObstaculo = gerarFrequenciaObstaculo(1)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel4' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel4' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 1 * 1000);
+        break;
+        case 5:           
+            camada0.style.backgroundColor = 'rgba(0, 0, 0, 0.57)'   
+            frequenciaObstaculo = gerarFrequenciaObstaculo(1)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel5' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel5' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 1 * 1000);
+        break;
+        case 6:
+            camada0.style.backgroundColor = 'rgba(0, 0, 0, 0.82)' 
+            frequenciaObstaculo = gerarFrequenciaObstaculo(1)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel6' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel6' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 0.5 * 1000);
+        break;
+        case 7:
+            camada0.style.backgroundColor = 'rgba(0, 0, 0, 0.82)' 
+            frequenciaObstaculo = gerarFrequenciaObstaculo(1)
+            if(frequenciaObstaculo === 1){
+                if(localObstaculo === 1){
+                    novo.classList.add('obstaculo', 'obstaculoNivel7' ,'obstaculoCima')
+                    container.appendChild(novo)
+                }else{
+                    novo.classList.add('obstaculo', 'obstaculoNivel7' ,'obstaculoBaixo')
+                    container.appendChild(novo)
+                }                
+            }
+            setTimeout(play, 0.5 * 1000);
+        break;
+    }
+    novo.addEventListener('animationend', () => {
+        novo.remove();
+    });
+}
+
+
+play()
+
+function gerarFrequenciaObstaculo(max){
+    return Math.floor(Math.random() * max) + 1;
+}
+
+function gerarLocalObstaculo(){
     return Math.floor(Math.random() * 2) + 1;
 }
 
-
-function teste() {
-    if(!rodando) return
-    if(pontos == 10) dificuldade = 2
-    if(pontos == 20) dificuldade = 3
-    if(pontos == 30) dificuldade = 4
-    if(pontos == 40) dificuldade = 5
-    if(pontos == 50) dificuldade = 6
-    if(pontos == 60) dificuldade = 7
-    if(pontos == 70) dificuldade = 8
-    if(pontos == 80) dificuldade = 9
-    if(pontos == 90) dificuldade = 10
-    tipoObstaculo = gerar()
-
-    pontos++
-    document.getElementById('pontos').textContent = pontos
-    
-    switch(dificuldade){
-        case 1:
-            span = 2
-        break;
-
-        case 2:
-            span = 1
-            classeObstaculo  = 'obstaculo-mover2'
-        break;
-
-        case 3:
-            span = 0.7
-            classeObstaculo  = 'obstaculo-mover3'
-        break;
-
-        case 4:
-            span = 0.7
-            classeObstaculo  = 'obstaculo-mover4'
-        break;
-
-        case 5:
-            span = 0.7
-            classeObstaculo  = 'obstaculo-mover5'
-        break;
-
-        case 6:
-            span = 0.7
-            classeObstaculo  = 'obstaculo-mover6'
-        break;
-
-        case 7:
-            span = 0.6
-            classeObstaculo  = 'obstaculo-mover7'
-        break;
-
-        case 8:
-            span = 0.5
-            classeObstaculo  = 'obstaculo-mover8'
-        break;
-
-        case 9:
-            span = 0.4
-            classeObstaculo  = 'obstaculo-mover9'
-        break;
-
-        case 10:
-            span = 0.3
-            classeObstaculo  = 'obstaculo-mover10'
-        break;
-
-    }    
-    
-    const tempo = Math.floor(Math.random() * 4);
-
-    let nova = document.createElement('div');
-
-    if(tipoObstaculo == 1){
-        nova.classList.add('obstaculo', classeObstaculo);
-        cenario.appendChild(nova);
-        setTimeout(teste, span * 1000);
-
-    }else{
-        nova.classList.add('obstaculo', classeObstaculo, 'obstaculo-2');
-        cenario.appendChild(nova);
-        setTimeout(teste, span * 1000);
-    }
-    
-}
-
- teste()
-
-
-// ===== DETECÇÃO DE COLISÃO =====
-
-// função de colisão
 function colidiu(el1, el2) {
     const a = el1.getBoundingClientRect();
     const b = el2.getBoundingClientRect();
@@ -141,24 +202,52 @@ function colidiu(el1, el2) {
     );
 }
 
-// loop de verificação (ajuste as classes se necessário)
 setInterval(() => {
-    const player = document.querySelector(".personagem");
-    const obstaculos = document.querySelectorAll(".obstaculo");
 
-    if (!player) return;
+    if (!personagem || !podeColidir) return;
 
-    obstaculos.forEach(obs => {
-        if (colidiu(player, obs)) {
-            console.log("COLIDIU");
-            rodando = false
+    const elementos = document.querySelectorAll('.obstaculo');
 
-            const elementos = document.querySelectorAll(".obstaculo");
+    for (let obs of elementos) {
+        if (colidiu(personagem, obs)) {
 
-            elementos.forEach(el => {
-                el.remove();
-            });
+            explosao()
 
+            podeColidir = false;
+            podeMover = false
+
+            setTimeout(() => {
+                podeMover = true;
+            }, 1000);
+
+            setTimeout(() => {
+                podeColidir = true;
+            }, 3000);
+
+            break; // para o loop após colisão
         }
-    });
+    }
+
 }, 50);
+
+function explosao(){
+     personagem.classList.add('personagemCapote')
+        personagemImg.src = 'img/personagemExplosao1.png'
+        setTimeout(()=>{
+            personagemImg.src = 'img/personagemExplosao1.png'
+        },60)
+        setTimeout(()=>{
+            personagemImg.src = 'img/personagemExplosao2.png'
+        },60)
+        setTimeout(()=>{
+            personagemImg.src = 'img/personagemExplosao3.png'
+        },60)
+        setTimeout(()=>{
+            personagemImg.src = 'img/personagemExplosao4.png'
+        },60)
+        setTimeout(() => {
+                personagem.classList.remove('personagemCapote')
+                personagemImg.src = 'img/personagem.png'
+
+            }, 1000);
+}
